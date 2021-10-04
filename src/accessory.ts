d2000542e0483b247e94ae828a324745237f199a
Ludvig Ericson <ludvig@lericson.se>
2021-10-04 17:46:30 +0200

    Robustness is the powerhouse of the cell

--- src/accessory.ts
+++ src/accessory.ts
@@ -12,6 +12,7 @@ import {
 } from "homebridge";
 
 import * as hue from "node-hue-api";
+import { setTimeout } from "timers";
 
 let hap: HAP;
 
@@ -28,13 +29,13 @@ class MetaLight implements AccessoryPlugin {
   private readonly log: Logging;
   private readonly name: string;
   private lock = false;
+  private getLightsOp: Promise<any[]> | null = null;
   private hue: any;
 
   private readonly lightbulbService: Service;
   private readonly informationService: Service;
 
   constructor(log: Logging, config: AccessoryConfig, api: API) {
-
     hue.api.createLocal(config.host).connect(config.username)
       .then(api => this.hue = api)
       .catch(err => console.error(`Unexpected Error: ${err.message}`));
@@ -42,31 +43,63 @@ class MetaLight implements AccessoryPlugin {
     this.log = log;
     this.name = config.name;
     
+    const delay: ((ms: number) => Promise<null>) = async (ms) =>
+      new Promise<null>((resolve, reject) => {
+        setTimeout(resolve, ms);
+      });
+    
+    const getAllLights: (() => Promise<any[]>) = async () => {
+      if (this.getLightsOp) {
+        log.debug('getAllLights: already in-progress, awaiting');
+        return await this.getLightsOp;
+      } else {
+        this.getLightsOp = (async (): Promise<any[]> => {
+          log.debug('getAllLights: begin fetching all lights');
+          let allLights: any[] = await this.hue.lights.getAll();
+          log.debug('getAllLights: finished fetching');
+          return allLights;
+        })();
+        try {
+          return await this.getLightsOp;
+        } finally {
+          setTimeout(() => { this.getLightsOp = null; }, 100);
+        }
+      }
+    };
+    
     const getAverage: (() => Promise<number>) = async () => {
-      log.debug('getAverage: begin fetching all lights');
-      let allLights: any[] = await this.hue.lights.getAll();
-      log.debug('getAverage: finished fetching');
-      let brightness: number[] = 
-        allLights
-          .filter(light => light.state.on)
-          .map(light => light.state.bri);
+      let allLights: any[] = await getAllLights();
+      let brightness: number[] = allLights
+        .filter(light => light.state.on && light.state.bri > 1)
+        .map(light => light.state.bri);
       if (brightness.length == 0) {
         return 0;
       }
+      log.debug(`brightnesses: ${brightness.join(", ")}`);
       return brightness.reduce(((sum, b) => sum + b), 0) / brightness.length;
     };
 
     const setStates: ((newState: any) => Promise<any>) = async (newState: any) => {
       log.debug('setStates: begin fetching all lights');
-      let allLights: any[] = await this.hue.lights.getAll();
+      let allLights: any[] = await getAllLights();
       log.debug('setStates: finished fetching, setting states');
       if (!this.lock) {
         this.lock = true;
         try {
           await Promise.all(
             allLights
-              .filter(light => light.state.on)
-              .map(light => this.hue.lights.setLightState(light, newState))
+              .filter(light => light.state.on && ((newState.on === false) || light.state.bri > 1))
+              .map(async (light, i) => {
+                if (i > 2) {
+                  await delay((i-2)*200);
+                }
+                log.debug(`setStates: updating #${light.id} ${light.name} state`);
+                try {
+                  await this.hue.lights.setLightState(light, newState);
+                } catch (err) {
+                  log.error(`failed to set state of #${light.id} ${light.name}: ${err}`)
+                }
+              })
           );
         } finally {
           this.lock = false;
@@ -80,11 +113,16 @@ class MetaLight implements AccessoryPlugin {
     this.lightbulbService = new hap.Service.Lightbulb(this.name);
 
     this.lightbulbService.getCharacteristic(hap.Characteristic.On)
-      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) =>
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+        log.debug('get on characteristic');
         getAverage()
           .then((average: number) => callback(undefined, average > 0))
-          .catch((err: any) => callback(err, undefined)))
-      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+          .catch((err: Error) => {
+            log.error(`Get ON error, ${err.name}: ${err.message}`);
+            callback(err, undefined);
+          });
+        log.debug('get on characteristic done');
+      }).on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
         if (value as boolean) {
           // Ignore turning on.
           callback();
@@ -96,11 +134,16 @@ class MetaLight implements AccessoryPlugin {
       });
 
     this.lightbulbService.getCharacteristic(hap.Characteristic.Brightness)
-      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) =>
+      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+        log.debug('get brightness characteristic');
         getAverage()
           .then((brightness: number) => callback(undefined, brightness/254*100))
-          .catch((err: any) => callback(err, undefined)))
-      .on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
+          .catch((err: Error) => {
+            log.error(`Get Brightness error, ${err.name}: ${err.message}`);
+            callback(err, undefined);
+          });
+        log.debug('get brightness characteristic done');
+      }).on(CharacteristicEventTypes.SET, (value: CharacteristicValue, callback: CharacteristicSetCallback) => {
         callback(undefined, undefined);
         setStates({bri: (value as number) / 100 * 254})
           .catch((err: Error) => log.error(`${err.name}: ${err.message}`));
